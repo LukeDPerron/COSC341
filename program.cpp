ac46426b839c4048d588df3c442115943b152a1b
Luke Perron <LukeDPerron@users.noreply.github.com>
2025-11-18 23:20:03 -0500

    added a Score class

--- program.cpp
+++ program.cpp
@@ -20,7 +20,9 @@ double compute_pi(int n)
 
 void scores()
 {
+    //user input
     string input = "";
+    //initializing
     string min_student = "";
     string max_student = "";
 
@@ -37,13 +39,8 @@ void scores()
 
     while(true)
     {
-        cout << "Enter student name and score, or -1 to quit: ";
+        cout << "Enter score, or -1 to quit: ";
 
-        cin >> input;
-        if(input == "-1")
-        {
-            break;
-        }
         cin >> score;
         if(score == -1)
         {
@@ -53,13 +50,11 @@ void scores()
        // if score is less than min, change min to score, and copy the name of the person who got mine to min_student
         if (score < min_score)
         {
-            min_student = input;
             min_score = score;
         }
         // if score is greater than max, change max to score, and copy the name of the person who got max to max_student
         if (score > max_score)
         {
-            max_student = input;
             max_score = score;
         }
         // increasing total score variable to be divided later.
@@ -72,9 +67,9 @@ void scores()
     {
         // average score using total scores divided by the count
         double avg_score = total / count;
-        // getting avg score up to two decimals
+        // getting avg score 
         cout << "Avg score: " << avg_score << "\n";
-        cout << "Max penis score: " << max_score << "\n";
+        cout << "Max score: " << max_score << "\n";
         cout << "Min score: " << min_score << "\n";
     }
     // if no scores are entered, tell user.
@@ -87,20 +82,22 @@ void scores()
 
 void file_sort(char *infile, char *outfile)
 {
+    //getting infile
     ifstream fin(infile);
-
+    //creating students variable
     int students = 0;
-
+    //getting input from file and putting in amount of students
     fin >> students;
+    //dynamically allocating arrays for each variable
     int *student_ids = new int [students];
     char *grades = new char [students];
     double *gpa = new double [students];
-
+    //looping through the file and storing the values in respective arrays
     for (int i = 0; i < students; i++)
     {
         fin >> student_ids[i] >> grades[i] >> gpa[i];
     }
-
+    //closing file pointer
     fin.close();
 
     //sorting based on ids
@@ -108,6 +105,7 @@ void file_sort(char *infile, char *outfile)
     {
         for(int j = i + 1; j < students; j++)
         {
+            //if one id is less than the other, switch all values
             if (student_ids[j] < student_ids[i])
             {  
                 int tempid = student_ids[i];
@@ -124,17 +122,160 @@ void file_sort(char *infile, char *outfile)
             }
         }
     }
+    //creating file pointer to output file
     ofstream fout (outfile);
-
+    //printing number of students
     fout << students << "\n";
-
+    //printing each value in ascending order
     for(int i = 0; i < students; i++)
     {
         fout << student_ids[i] << " " << grades[i] << " " << gpa[i] << "\n";
     }
-
+    //closing pointer
     fout.close();
 
+}
+
+//triangle class
+class Rectangle
+{
+    //private variables
+    private:
+        int width;
+        int height;
+    //public constructors and methods
+    public:
+    //creating method declerations
+        Rectangle();
+        Rectangle(int size);
+        Rectangle(int width, int height);
+
+        void setWidth(int width);
+        void setHeight(int height);
+
+        int getWidth();
+        int getHeight();
+
+        int area();
+        void display();
+};
+
+//constructor with no args setting both to 0
+Rectangle::Rectangle()
+{
+    width = 0;
+    height = 0;
+}
+
+//constructor with 1 arg setting both to it
+Rectangle::Rectangle(int size)
+{
+    width = size;
+    height = size;
+}
+
+//constructor with two args setting to respective value
+Rectangle::Rectangle(int width, int height)
+{
+    this->width = width;
+    this->height = height;
+}
+//setting width
+void Rectangle::setWidth(int width)
+{
+    this->width = width;
+}
+//setting height
+void Rectangle::setHeight(int height)
+{
+    this->height = height;
+}
+//getting width
+int Rectangle::getWidth()
+{
+    return width;
+}
+//getting height
+int Rectangle::getHeight()
+{
+    return height;
+}
+//returning area
+int Rectangle::area()
+{
+    return (width * height);
+}
+//printing height and width
+void Rectangle::display()
+{
+    cout << "Height: " << height << "\nWidth: " << width;
+}
+
+//score class
+class Score
+{
+    //private variavles
+    private:
+        double *scores;
+        int numStudents;
+    //public constructors and methods
+    public:
+    //method decleration
+        Score(double inputScores[], int numStudents);
+        ~Score(); 
+
+        double average();
+
+        void display();
+        
+};
+
+//constuctor that creates array of student scores
+Score::Score(double inputScores[], int numStudents)
+{
+    //setting method variable to input
+    this->numStudents = numStudents;
+    //allocating scores private variable
+    scores = new double[numStudents];
+    //looping through the given array and putting into private variable
+    for(int i = 0; i < numStudents; i++)
+    {
+        scores[i] = inputScores[i];
+    }
+}
+
+//destructor
+Score::~Score()
+{
+    delete [] scores;
+}
+
+//average method
+double Score::average()
+{
+    //inital average variable
+    double average = 0;
+    //adding each number to average
+    for (int i = 0; i < numStudents; i++)
+    {
+        average += scores[i];
+    }
+    //returning average / numStudents to get real average
+    return (average / numStudents);
+}
+
+void Score::display()
+{
+    //printing out each score with a space inbetween
+    cout << "Scores: ";
+    for(int i = 0; i < numStudents; i++)
+    {
+        cout << scores[i] << " ";
+    }
+    //newline for style
+    cout << "\n";
+    //printing out average
+    cout << "Average: " << average();
 }
 
 int main()
@@ -145,13 +286,19 @@ int main()
     // cout << "Your value of pi using " << input << " is " << compute_pi(input);
 
     // scores();
-    string input_file;
-    string output_file;
-    cout << "Enter name of input file: \n";
-    cin >> input_file;
-    cout << "Enter name of output file: \n";
-    cin >> output_file;
-    file_sort(&input_file[0], &output_file[0]);
+
+    // string input_file;
+    // string output_file;
+    // cout << "Enter name of input file: \n";
+    // cin >> input_file;
+    // cout << "Enter name of output file: \n";
+    // cin >> output_file;
+    // file_sort(&input_file[0], &output_file[0]);
+
+    double arr[] = {85.5, 90.0, 78.5, 92.0};
+    Score s(arr, 4);
+
+    s.display();
 
     return 0;
 }
